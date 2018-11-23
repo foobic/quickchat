a2a18d697ce7ef6f19127b0580f68148e59de149
foobic <foobic@gmail.com>
2018-11-24 00:26:09 +0200

    small html makeup added

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,10 @@
-'use strict'
-
 module.exports = {
-  presets: [
-    '@vue/app'
+  "presets": [
+    [
+      "@vue/app",
+      {
+        "useBuiltIns": "entry"
+      }
+    ]
   ]
 }
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,51 @@
 <template>
   <div id="app">
-    init
-    <home></home>
+    <div class="container">
+      <div class="logo d-flex justify-content-center mt-4">
+        <span>QuickChat</span>
+      </div>
+      <div class="form-group d-flex justify-content-center ">
+        <!--<div class="borderWrapper">-->
+          <!--&lt;!&ndash;<input type="text"  >&ndash;&gt;-->
+          <!--<div class="w-50">-->
+            <!--<input class="form-control form-control-lg createBtn noFocus"  placeholder="New room name" type="text">-->
+          <!--</div>-->
+          <!--&lt;!&ndash;<div class="w-25">&ndash;&gt;-->
+          <!--&lt;!&ndash;<button class="btn btn-primary btn-lg noFocus w-100">Create</button>&ndash;&gt;-->
+          <!--&lt;!&ndash;</div>&ndash;&gt;-->
+          <!--<button class="btn btn-primary btn-lg noFocus">Create</button>-->
+        <!--</div>-->
+        <div class="input-group mb-3">
+          <input class="form-control form-control-lg createInput noFocus"  placeholder="New room name" type="text" aria-label="Recipient's username" aria-describedby="basic-addon2">
+          <!--<input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">-->
+          <div class="input-group-append">
+            <button class="btn btn-primary btn-lg noFocus createBtn">Create</button>
+            <!--<button class="btn btn-outline-secondary" type="button">Button</button>-->
+          </div>
+        </div>
+      </div>
+      <div class="newRoom form-group">
+      </div>
+      sdfsdf
+      <home></home>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+  @import 'common';
+
+  .logo > span {
+    font-size: 3em;
+    color: $mainColor;
+  }
+  .createInput{
+    border-color: $mainColor;
+    &::placeholder{
+      color: $mainColor;
+      opacity: 0.9;
+    }
+  }
 
 </style>
 
