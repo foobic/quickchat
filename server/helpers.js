7bc0a13a9157e7b7ec13beae4cf67a3e9fa3c4a5
foobic <foobic@gmail.com>
2019-02-05 23:26:57 +0200

    Server decomposition

--- server/helpers.js
+++ server/helpers.js
@@ -10,4 +10,26 @@ const receiveData = (req, cb) => {
     });
 };
 
-module.exports = {receiveData};
+const getNicknames = set => {
+  const list = [];
+  set.forEach(el => {
+    list.push(el.nickname);
+  });
+  return list;
+};
+
+const stringifyObjectKeys = object => JSON.stringify(Object.keys(object));
+
+const getCurrentTime = () =>
+  new Date().toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  });
+
+module.exports = {
+  receiveData,
+  getNicknames,
+  stringifyObjectKeys,
+  getCurrentTime,
+};
--- server/index.js
+++ server/index.js
@@ -1,91 +1,87 @@
 const http = require('http');
 const WebSocket = require('ws');
 const url = require('url');
-const {receiveData} = require('./helpers');
-const logger = require('./logger');
 
-const rooms = {};
-const roomListSocket = new WebSocket.Server({noServer: true});
-let msg = null;
+const rooms = {}; // '/roomname' : WS
 
-function getNicknames(set) {
-  const list = [];
-  set.forEach(el => {
-    list.push(el.nickname);
-  });
-  return list;
-}
+const {
+  receiveData,
+  getNicknames,
+  stringifyObjectKeys,
+  getCurrentTime,
+} = require('./helpers');
+const logger = require('./logger.js');
+const {roomListSocket} = require('./roomlist.js')(rooms);
 
-roomListSocket.broadcast = function broadcast(data) {
-  roomListSocket.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(data));
+const notifyRoomListUpdated = () =>
+  roomListSocket.broadcast(stringifyObjectKeys(rooms));
+
+const checkRoomIsAlive = () => {
+  const deletionList = [];
+  Object.keys(rooms).forEach(el => {
+    if (rooms[el].clients && rooms[el].clients.size === 0) {
+      deletionList.push(rooms[el].roomname);
     }
   });
+  deletionList.forEach(el => {
+    logger({text: `Room ${el} deleted.`});
+    delete rooms[el];
+  });
+  if (deletionList.length > 0) {
+    notifyRoomListUpdated();
+  }
 };
 
-roomListSocket.on('connection', ws => {
-  ws.send(JSON.stringify(Object.keys(rooms)));
-  ws.on('close', () => {
-    logger({text: 'Roomlist connection closed.'});
-  });
-});
-
-const onConnection = wss => ws => {
+const onRoomConnection = wss => ws => {
   ws.on('message', message => {
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        msg = {
+        const msg = {
           nickname: ws.nickname,
           body: message,
-          time: new Date().toLocaleString('en-US', {
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-          }),
+          time: getCurrentTime(),
         };
         client.send(JSON.stringify(msg));
       }
     });
   });
 
-  console.log(getNicknames(wss.clients));
-
   ws.on('close', () => {
     if (wss.clients && wss.clients.size === 0) {
-      logger({text: `Room ${wss.roomName} deleted.`});
-      delete rooms[wss.roomName];
-      roomListSocket.broadcast(Object.keys(rooms));
+      delete rooms[wss.roomname];
+      logger({text: `Room ${wss.roomname} deleted.`});
+      notifyRoomListUpdated();
     }
   });
 };
+
+// every 1m cleanup room list if needed
+setTimeout(() => {
+  checkRoomIsAlive();
+}, 60000);
 
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const urlParsed = url.parse(req.url);
   if (req.method === 'POST' && urlParsed.pathname === '/create') {
     receiveData(req, body => {
-      const roomName = `/${body.name}`;
-      if (roomName in rooms) {
-        res.end(`'${roomName}' already exists.`);
-      }
+      const roomname = `/${body.name}`;
       const wss = new WebSocket.Server({noServer: true});
-      wss.roomName = roomName;
-      rooms[roomName] = wss;
-      wss.on('connection', onConnection(wss));
-      roomListSocket.broadcast(Object.keys(rooms));
-      res.end(`'${roomName}' room successfully created.`);
+      wss.roomname = roomname;
+      rooms[roomname] = wss;
+      wss.on('connection', onRoomConnection(wss));
+      notifyRoomListUpdated();
+      logger({text: `'${roomname}' room successfully created.`});
+      res.end(`'${roomname}' room successfully created.`);
     });
   } else if (req.method === 'GET' && urlParsed.pathname === '/getRoomList') {
-    res.end(JSON.stringify(Object.keys(rooms)));
+    res.end(stringifyObjectKeys(rooms));
   }
 });
 
 server.on('upgrade', (request, socket, head) => {
   const parsedUrl = url.parse(request.url);
   const {pathname} = parsedUrl;
-  const nickname =
-    parsedUrl && parsedUrl.query ? parsedUrl.query.split('=')[1] : null;
 
   if (pathname === '/roomList') {
     roomListSocket.handleUpgrade(request, socket, head, ws => {
@@ -93,18 +89,15 @@ server.on('upgrade', (request, socket, head) => {
     });
   } else if (Object.prototype.hasOwnProperty.call(rooms, pathname)) {
     rooms[pathname].handleUpgrade(request, socket, head, ws => {
+      const nickname =
+        parsedUrl && parsedUrl.query ? parsedUrl.query.split('=')[1] : null;
       const nicknameList = getNicknames(rooms[pathname].clients);
       if (nicknameList.includes(nickname)) {
+        // if this nickname already taken
         return socket.destroy();
       }
-
       const client = ws;
       client.nickname = nickname;
-      // rooms[pathname].clients.map(el => {
-      //   console.log(el.nickname);
-      //   return el;
-      // });
-      console.log();
       rooms[pathname].emit('connection', client, request);
     });
   } else {
