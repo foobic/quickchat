65636be70ff9a95a1eacb133236ce15d79b5885c
foobic <foobic@gmail.com>
2019-05-11 23:14:42 +0300

    Server folder structurized

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     {
       name: 'backend',
-      script: 'server/index.js',
+      script: 'server/src/index.js',
       exec_interpreter: 'node',
       ignore_watch: ['node_modules'],
       autorestart: true,
